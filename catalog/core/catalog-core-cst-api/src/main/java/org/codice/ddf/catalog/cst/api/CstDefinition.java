e1c4c1e76a2439a7501edac7ad8d302aeb672cfb
clockard <chris.lockard@connexta.com>
2018-10-02 15:26:29 -0700

    DDF-0000 formatting updates

--- catalog/core/catalog-core-cst-api/src/main/java/org/codice/ddf/catalog/cst/api/CstDefinition.java
+++ catalog/core/catalog-core-cst-api/src/main/java/org/codice/ddf/catalog/cst/api/CstDefinition.java
@@ -1,21 +1,33 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.codice.ddf.catalog.cst.api;
 
 import ddf.catalog.data.Attribute;
-
 import java.util.Set;
 
 public interface CstDefinition {
-    String getName();
+  String getName();
 
-    String getNamespace();
+  String getNamespace();
 
-    float getVersion();
+  float getVersion();
 
-    CstTerm getTerm(String name);
+  CstTerm getTerm(String name);
 
-    Set<CstTerm> getTerms();
+  Set<CstTerm> getTerms();
 
-    Attribute toPreviousVersion(Attribute attribute);
+  Attribute toPreviousVersion(Attribute attribute);
 
-    Attribute fromPreviousVersion(Attribute attribute);
+  Attribute fromPreviousVersion(Attribute attribute);
 }
--- catalog/core/catalog-core-cst-api/src/main/java/org/codice/ddf/catalog/cst/api/CstService.java
+++ catalog/core/catalog-core-cst-api/src/main/java/org/codice/ddf/catalog/cst/api/CstService.java
@@ -1,18 +1,30 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
 package org.codice.ddf.catalog.cst.api;
 
 import ddf.catalog.data.Attribute;
-
 import java.util.Set;
 
 public interface CstService {
 
-    Set<CstDefinition> getCstDefinitions();
+  Set<CstDefinition> getCstDefinitions();
 
-    CstDefinition getCstDefinition(String name);
+  CstDefinition getCstDefinition(String name);
 
-    Attribute transform(Attribute attribute, CstDefinition from, CstDefinition to);
+  Attribute transform(Attribute attribute, CstDefinition from, CstDefinition to);
 
-    Attribute toCatalogTaxonomy(Attribute attribute, CstDefinition from);
+  Attribute toCatalogTaxonomy(Attribute attribute, CstDefinition from);
 
-    Attribute fromCatalogTaxonomy(Attribute attribute, CstDefinition to);
+  Attribute fromCatalogTaxonomy(Attribute attribute, CstDefinition to);
 }
